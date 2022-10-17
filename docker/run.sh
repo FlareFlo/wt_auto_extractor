e6765df6f3b70dbbc363cd6e93a67646669a4d4e
FlareFlo <mail@flareflo.dev>
2022-10-17 21:42:48 +0200

    Add auth to steam, gets blocked by guard atm

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-echo "Init"
+echo "Setting steam-auth"
+PWD=$(cat steam_pwd)
+USR=$(cat steam_usr)
 
-dotnet /DepotDownloader/DepotDownloader.dll -app 236390 -depot 236391 -manifest 6590543165547691358 -filelist "list.txt"
+dotnet /DepotDownloader/DepotDownloader.dll -app 236390 -depot 236391 -manifest 6590543165547691358 -filelist "list.txt" -username "$USR" -password "$PWD"
+ls -a
